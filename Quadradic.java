368858eaa65948e139b08dfde74992eb77e0ef8c
Caleb Schroder <doomskier@gmail.com>
2019-08-23 12:53:02 -0600

    Other folder, + chapters

---
+++ Quadradic.java
@@ -0,0 +1,17 @@
+public class Quadradic
+{  
+  public static void main ( String[] args )  
+  {
+    double x;
+    double Y;
+    x = 2;
+    Y = x * x - 2 * x + 8;
+    System.out.println(Y);
+    x = 3;
+    Y = x * x - 2 * x + 8;
+    System.out.println(Y);
+    x = 4;
+    Y = x * x - 2 * x + 8;
+    System.out.println(Y);
+  }
+}
