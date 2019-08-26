652c5121ec43e3329cd1aa6213392d565b4366e0
Caleb Schroder <doomskier@gmail.com>
2019-08-25 22:17:55 -0600

    average rainfall

---
+++ rainfall.java
@@ -0,0 +1,22 @@
+public class rainfall
+{  
+  public static void main ( String[] args )  
+  {
+    double a;
+    double m;
+    double j;
+    double avg;
+
+    a = 12;
+    m = 14;
+    j = 8;
+    avg = (a+m+j)/3;
+
+
+    
+    System.out.println(a);
+    System.out.println(m);
+    System.out.println(j);
+    System.out.println(avg);
+  }
+}
