9fc7ddc65e4073e2fccfc7338a19cf4f94ec0b66
Caleb Schroder <doomskier@gmail.com>
2019-08-27 21:55:10 -0600

    circle attempt

---
+++ c12/circle.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class circle
+{  
+  public static void main ( String[] args )  
+  {
+    Scanner scan =  new Scanner ( System.in );
+    double radius;
+
+    System.out.println("Enter the data:");
+
+    radius = scan.nextline();
+    circlelength = radius * Math.PI;
+
+    System.out.println("The circle is" + circlelength);
+}
+
+
+   
+}
