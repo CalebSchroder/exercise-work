1cc7caf5874f68093124ea9e7aec6fbd99936e9a
Caleb Schroder <doomskier@gmail.com>
2019-09-06 12:32:10 -0600

    exercise 1 ch 16

---
+++ c16/Discount.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+public class Discount {
+
+    public static void main(String[]args) {
+
+        Scanner scan = new Scanner (System.in);
+        double p;
+        double fp;
+    
+
+        System.out.println("Enter the orignal price in dollars");
+        p = scan.nextDouble();
+
+        fp = p * 0.9;
+
+        System.out.println("The Discounted price is $" + fp);
+
+
+
+        
+        
+
+
+
+
+
+    }
+
+
+
+
+
+}
