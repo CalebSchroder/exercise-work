e747f5df0f880875a2ad578f14d69bb7b2c229ea
Caleb Schroder <doomskier@gmail.com>
2019-09-10 16:27:45 -0600

    c17 exer 5

---
+++ c17/Dump.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+public class Dump {
+
+	public static void main(String[]args) {
+
+		Scanner scan = new Scanner (System.in);
+		
+		int weight;
+        int cost;
+       
+
+        
+
+		System.out.println("Enter the weight of the trash in pounds:");
+        weight = scan.nextInt();
+
+        cost = 20;
+        if (weight > 200) {
+            cost = cost + (((weight - 200) / 100) * 8);
+        }
+
+        System.out.println("The cost is $" + cost);
+
+
+        
+
+
+
+
+	}
+
+
+
+}
