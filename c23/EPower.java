5ffbe00b07b4be48828983d5a5a12ebede6d86f0
Caleb Schroder <doomskier@gmail.com>
2019-09-17 22:43:11 -0600

    c23 almost done

---
+++ c23/EPower.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+import java.lang.Math;
+public class EPower {
+
+
+    public static void main(String[] args) {
+        
+        Scanner scan = new Scanner(System.in);
+        Double x;
+        Double sum;
+        int n;
+        Double real;
+
+        sum = 1 + x;
+        
+
+        while ((math.pow(x, n) / (n)!) >= 1.0E-12) {
+            sum = (sum + math.pow(x, n)) / (n)!;
+            n = n + 1;
+        }
+        System.out.println("My: " + sum);
+        real = Math.exp(x);
+        System.out.println("Real" + real);
+        
+
+      
+     
+      
+         
+
+
+     }
+}
