d89c2a8babadea83e40c7d381af5df2036a9e38c
Caleb Schroder <doomskier@gmail.com>
2019-09-30 12:52:33 -0600

    exer 1 c30

---
+++ c30/GeussingGame.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class GeussingGame {
+    public static void main (String[]args) {
+        Random rand = new Random;
+        Scanner scan = new Scanner(System.in);
+
+        int n;
+        int g;
+        n = rand.nextInt();
+        g = scan.nextInt();
+
+    }
+}
