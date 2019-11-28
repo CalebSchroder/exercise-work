5578defc74193b9fb3653fb6092040afa5c9ef31
CalebSchroder <doomskier@gmail.com>
2019-11-27 17:42:48 -0700

    exer1 done

---
+++ c60/Exercise1.java
@@ -0,0 +1,7 @@
+public class Exercise1 {
+	public static void main (String[]args) {
+		int[] val = {0, 1, 2, 3};
+		int sum = val[0] + val[1] + val[2] + val[3];
+		System.out.println("Sum of all the numbers = " + sum);
+	}
+}
