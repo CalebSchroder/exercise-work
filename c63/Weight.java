ef9150a142194c17d49e24e8d857158775ddf7ff
CalebSchroder <doomskier@gmail.com>
2019-11-29 14:43:51 -0700

    Almost done

---
+++ c63/Weight.java
@@ -0,0 +1,25 @@
+class Weight
+{
+  private int[] data;
+  
+  // Constructor
+  public Weight(int[] init) {
+    // Make data the same length
+    // as the array referenced by init.
+    data = new ....
+    
+    // Copy values from the 
+    // input data to data.
+    for (int j = 0; j <  ; j ++)
+    {
+      data[j] = 
+    }
+  }
+  
+  //Print
+  public String toString()
+  {
+ 
+ 
+  }
+}
